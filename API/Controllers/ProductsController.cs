1234516fb5444eea653575281123b8948e472358
Tim <qitingliu7@gmail.com>
2021-09-04 19:49:06 -0400

    Api paging filtering sorting searching: adding filtering functionality

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -35,9 +35,10 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(
+            string sort, int? brandId, int? typeId)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
             var products = await _productsReop.ListAsync(spec);
 
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,25 +6,31 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification(string sort)
+        public ProductsWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId)
+        : base(x =>
+               (!brandId.HasValue || x.ProductBrandId == brandId) &&
+               (!typeId.HasValue || x.ProductTypeId == typeId)
+
+        )
+
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
             AddOrderBy(x => x.Name);
 
-            if(!string.IsNullOrEmpty(sort))
+            if (!string.IsNullOrEmpty(sort))
             {
-                switch(sort)
+                switch (sort)
                 {
                     case "priceAsc":
-                       AddOrderBy(p => p.Price);
-                       break;
+                        AddOrderBy(p => p.Price);
+                        break;
                     case "priceDesc":
-                       AddOrderByDescending(p => p.Price);
-                       break;
+                        AddOrderByDescending(p => p.Price);
+                        break;
                     default:
-                       AddOrderBy(n => n.Name);
-                       break;
+                        AddOrderBy(n => n.Name);
+                        break;
                 }
             }
         }
