68586671963a78b53904952c96b8cbfc90df517c
Tim <qitingliu7@gmail.com>
2021-09-03 20:41:34 -0400

    Api error handling: cleaning up the Startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,7 @@ using Core.Interfaces;
 using API.Helpers;
 using API.Middleware;
 using API.Errors;
+using API.Extensions;
 
 namespace API
 {
@@ -33,42 +34,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+          
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.Configure<ApiBehaviorOptions>(options =>
-             {
-                 options.InvalidModelStateResponseFactory = actionContext =>
-                 {
-                     var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-                     var errorResponse = new ApiValidationErrorResponse
-                     {
-                         Errors = errors
-
-                     };
-
-                     return new BadRequestObjectResult(errorResponse);
-                 };
-
-             });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+
             // if (env.IsDevelopment())
             // {
             //     // app.UseDeveloperExceptionPage();
@@ -82,6 +62,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
