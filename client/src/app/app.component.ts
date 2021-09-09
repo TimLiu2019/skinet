039965a9ad412002b7b67281b7e40b8044aee74d
Tim <qitingliu7@gmail.com>
2021-09-09 16:14:00 -0400

    Creating a products interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   
   title = 'Skinet';
-  products: any[] = [];
+  products: IProduct[];
   constructor(private http: HttpClient){
 
   }
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?PageSize=50').subscribe((response:any)=>{
-      console.log(response);
+    this.http.get('https://localhost:5001/api/products?PageSize=50')
+    .subscribe(
+      (response:IPagination )=>{
+     // console.log(response );
       this.products = response.data;
     },error =>{
       console.log(error);
