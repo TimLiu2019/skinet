0c289fd320a274fc947a9776ed94da219c9aa08d
Tim <qitingliu7@gmail.com>
2021-09-15 19:50:38 -0400

    Creating additional error components: not-found & server-error

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'test-error', component: TestErrorComponent},
+  {path:'server-error', component: ServerErrorComponent},
+  {path:'not-found', component: NotFoundComponent},
 
   // lazy loading, load module when click shop
   {path:'shop',loadChildren: ()=>import('./shop/shop.module').then(mod => mod.ShopModule)},
@@ -19,6 +23,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
