7ae6907473dea762f939375e95cf85e06302a599
Tim <qitingliu7@gmail.com>
2021-09-22 21:04:06 -0400

    Creating the basket module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -16,6 +16,8 @@ const routes: Routes = [
   // lazy loading, load module when click shop
   {path:'shop',loadChildren: ()=>import('./shop/shop.module').then(mod => mod.ShopModule),
   data: {breadcrumb: 'Shop'}},
+  {path:'basket',loadChildren: ()=>import('./basket/basket.module').then(mod => mod.BasketModule),
+  data: {breadcrumb: 'Basket'}},
  // {path:'shop', component: ShopComponent},
  // {path:'shop/:id', component: ProductDetailsComponent},
   {path:'**',redirectTo:'not-found',pathMatch:'full'}
