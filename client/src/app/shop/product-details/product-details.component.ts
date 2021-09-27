b5a8a225c3436b9a66f098a2d90cf411a7d3fcb4
Tim <qitingliu7@gmail.com>
2021-09-26 21:08:49 -0400

    Hooking up the product detail component to the basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { IProduct } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
+import { BasketService } from '../../basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,21 +12,36 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
 
   constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
-              private bcService: BreadcrumbService ) { 
-                this.bcService.set('@productDetails','');
-              }
+    private bcService: BreadcrumbService, private basketService: BasketService) {
+    this.bcService.set('@productDetails', '');
+  }
 
-  ngOnInit()  {
+  ngOnInit() {
     this.loadProduct();
   }
-  
+
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity(){
+    this.quantity++;
+  }
+
+  decrementQuantity(){
+    if(this.quantity > 1){
+      this.quantity--;
+    }
+  }
+
   loadProduct() {
     this.shopService.getProduct(+ this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
-      this.bcService.set('@productDetails',product.name);
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error)
     });
