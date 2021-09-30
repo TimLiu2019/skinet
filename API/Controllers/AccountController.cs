eb13ec58b9f927dc94399b2901f60e49adee0129
Tim <qitingliu7@gmail.com>
2021-09-30 19:43:35 -0400

    Adding another Dto to the user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -20,10 +21,12 @@ namespace API.Controllers
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
 
+        private readonly IMapper _mapper;
 
         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
-         ITokenService tokenService)
+         ITokenService tokenService, IMapper mapper)
         {
+            _mapper = mapper;
             _signInManager = signInManager;
             _userManager = userManager;
             _tokenService = tokenService;
@@ -35,7 +38,7 @@ namespace API.Controllers
         {
             var email = User.FindFirstValue(ClaimTypes.Email);
             var user = await _userManager.FindByEmailFromClaimsPrinciple(User);
-             return new UserDto
+            return new UserDto
             {
                 DisplayName = user.DisplayName,
                 Token = _tokenService.CreateToken(user),
@@ -47,28 +50,42 @@ namespace API.Controllers
         [HttpGet("emailexists")]
         public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
         {
-           return await _userManager.FindByEmailAsync(email) != null;
+            return await _userManager.FindByEmailAsync(email) != null;
         }
         [Authorize]
         [HttpGet("address")]
-        public async Task<ActionResult<Address>> GetUserAddress()
+        public async Task<ActionResult<AddressDto>> GetUserAddress()
         {
-            
-             var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(User);
-             return user.Address;
-            
+
+            var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(User);
+            return _mapper.Map<Address, AddressDto>(user.Address);
+
         }
-        
+
+        [Authorize]
+        [HttpPut("address")]
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
+        {
+            var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(User);
+            user.Address = _mapper.Map<AddressDto, Address>(address);
+
+            var result = await _userManager.UpdateAsync(user);
+            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+            return BadRequest("Problem updating the user");
+        }
+
+       
+
 
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _userManager.FindByEmailAsync(loginDto.Email);
-            if(user == null) return Unauthorized(new ApiResponse(401));
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password,false);
+            if (user == null) return Unauthorized(new ApiResponse(401));
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
-            if(!result.Succeeded) return Unauthorized(new ApiResponse(401));
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
             return new UserDto
             {
                 Email = user.Email,
@@ -78,7 +95,7 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Regiser(RegisterDto registerDto) 
+        public async Task<ActionResult<UserDto>> Regiser(RegisterDto registerDto)
         {
             var user = new AppUser
             {
@@ -87,8 +104,8 @@ namespace API.Controllers
                 UserName = registerDto.Email
 
             };
-            var result =await _userManager.CreateAsync(user,registerDto.Password);
-            if(!result.Succeeded) return BadRequest(new ApiResponse(400));
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
             return new UserDto
             {
                 DisplayName = user.DisplayName,
@@ -102,6 +119,6 @@ namespace API.Controllers
 
 
 
-    
+
     }
 }
