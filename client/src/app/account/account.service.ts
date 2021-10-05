2862867b5de6bb5024b77db9a4a54c2f24de24fd
Tim <qitingliu7@gmail.com>
2021-10-04 21:29:05 -0400

    Creating the account service

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { IUser } from '../shared/models/user';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<IUser>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+  
+  constructor(private http: HttpClient, private router: Router) { }
+  login(values: any) {
+    return this.http.post(this.baseUrl + 'account/login', values).pipe(
+      map((user:IUser) => {
+        if(user){
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
 
-  constructor() { }
+      })
+    );
+  }
+
+  register(values:any){
+    return this.http.post(this.baseUrl + 'account/register',values).pipe(
+      map((user:IUser) =>{
+        if(user){
+          localStorage.setItem('token',user.token);
+        }
+      })
+    );
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string){
+    return this.http.get(this.baseUrl + '/account/emailexists?email='+email);
+  }
 }
