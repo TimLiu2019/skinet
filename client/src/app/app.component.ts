1e9a8857ab14db1790c7a8dc8824efb7d1cfcb8a
Tim <qitingliu7@gmail.com>
2021-10-06 21:41:03 -0400

    Persisting the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,25 @@ export class AppComponent implements OnInit{
   
   title = 'Skinet';
 
-  constructor(private basketService: BasketService){
+  constructor(private basketService: BasketService, private accountService: AccountService){
   }
   ngOnInit(): void {
+     this.loadBasket();
+     this.loadCurrentUser();
+   
+  }
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+    if(token){
+      this.accountService.loadCurrentUser(token).subscribe(()=>{
+        console.log('loaded user');
+      },error =>{
+        console.log(error);
+      })
+    }
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if(basketId){
       this.basketService.getBasket(basketId).subscribe(()=>{
@@ -21,6 +38,6 @@ export class AppComponent implements OnInit{
         console.log(error);
       });
     }
-   
+
   }
 }
