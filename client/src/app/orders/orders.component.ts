fc3116c146214beeb3b0387bc01ff54c3eee655e
Tim <qitingliu7@gmail.com>
2021-11-02 21:00:58 -0400

    orders component

--- client/src/app/orders/orders.component.ts
+++ client/src/app/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../shared/models/order';
+import { OrdersService } from './orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orderService: OrdersService) { }
+  orders: IOrder[];
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.getOrdersForUser().subscribe((orders: IOrder[]) => {
+      this.orders = orders;
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
