b74e933ee8091321b726856451f3376d075a42f4
Tim <qitingliu7@gmail.com>
2021-12-02 21:56:41 -0500

    Adding stripe elements to the components

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BasketService } from '../../basket/basket.service';
 import { CheckoutService } from '../checkout.service';
@@ -8,18 +8,47 @@ import { IOrder } from '../../shared/models/order';
 import { Router } from '@angular/router';
 import { NavigationExtras } from '@angular/router';
 
+declare var Stripe;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent implements OnInit {
+export class CheckoutPaymentComponent implements AfterViewInit,OnDestroy {
   @Input() checkoutForm: FormGroup;
+  @ViewChild('cardNumber', {static: true}) cardNumberElement :ElementRef;
+  @ViewChild('cardExpiry', {static: true}) cardExpiryElement :ElementRef;
+  @ViewChild('cardCvc', {static: true}) cardCvcElement :ElementRef;
+  stripe: any;
+  cardNumber: any;
+  cardExpiry: any;
+  cardCvc: any;
+  cardErrors: any;
 
   constructor(private basketService: BasketService, private checkoutService: CheckoutService,
          private toastr: ToastrService, private router: Router ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(){
+    this.stripe = Stripe('');
+    const elements = this.stripe.elements();
+
+    this.cardNumber = elements.create('cardNumber');
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+
+
+  }
+
+  ngOnDestroy (){
+    this.cardNumber.destory();
+    this.cardExpiry.destory();
+    this.cardCvc.destory();
   }
   submitOrder(){
     const basket = this.basketService.getCurrentBasketValue();
